b014723e5fea4106e2745e40dfc05c99b80cc2e1
Sunil Agarwal <sunil.agarwal@gmail.com>
2020-01-04 15:09:09 -0800

    4 Visualization for Economic Development Factors

--- world-insights/app.py
+++ world-insights/app.py
@@ -1,11 +1,8 @@
 # import necessary libraries
 import os
 from flask import Flask, render_template,  jsonify, request
-
-# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
-
 import numpy as np
-
+import pandas as pd
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -14,39 +11,46 @@ from sqlalchemy import create_engine, func
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
-rds_connection_string = "world-insights/economy_development_db.sqlite"
-#rds_connection_string = "economy_development_db.sqlite"
+#################################################
+# Database Setup
+#################################################
+rds_connection_string = "db/economy_development_db.sqlite"
 engine = create_engine(f'sqlite:///{rds_connection_string}')
 
+# reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
 session = Session(engine)
 
+# Save references to each table
+# economy_development = Base.classes.economy_development
+
+
 # Flask Routes
 #List all routes that are available
 @app.route("/")
 def welcome():
-     return (
-         f"/api/getDataFromYearToNow?indicator_code=<indicator_code>&year=<year><br/>"
-         f"/api/getDataForYear?indicator_code=<indicator_code>&year=<year><br/>"
-     )
+    """Return the homepage."""
+    return render_template("index.html")
+    # return (
+    #      f"/api/getDataFromYearToNow?indicator_code=<indicator_code>&year=<year><br/>"
+    #      f"/api/getDataForYear?indicator_code=<indicator_code>&year=<year><br/>"
+    #  )
 
 # Set route
 @app.route('/api/getDataFromYearToNow', methods=['GET'])
 def getDataFromYearToNow(indicator_code=None, year=None):
     args = request.args
-    print (args)# For debugging
+    print (args) # For debugging
     indicator_code = args['indicator_code']
     year = args['year']
-    # indicator_name = request.
-    # print(request.form.get('indicator_name'))
-    print(indicator_code)
-    print(year)
+    # print(indicator_code)
+    # print(year)
     
     sql_query = f"SELECT country_name, country_code, indicator_name, indicator_code, year, value   FROM economy_development where indicator_code='{indicator_code}' and year >='{year}'"
-    print(sql_query)
+    # print(sql_query)
     results = engine.execute(sql_query).fetchall()
 
     economy_development_data = []
@@ -72,11 +76,11 @@ def getDataForYear(indicator_code=None, year=None):
     year = args['year']
     # indicator_name = request.
     # print(request.form.get('indicator_name'))
-    print(indicator_code)
-    print(year)
+    # print(indicator_code)
+    # print(year)
     
     sql_query = f"SELECT country_name, country_code, indicator_name, indicator_code, year, value   FROM economy_development where indicator_code='{indicator_code}' and year ='{year}'"
-    print(sql_query)
+    # print(sql_query)
     results = engine.execute(sql_query).fetchall()
 
     economy_development_data = []
@@ -114,6 +118,16 @@ def economy_development(indicator_name, year=None):
         
     return jsonify(economy_development_data)
 
+# /api/getCountryNames?indicator_code=<indicator_code>
+@app.route('/api/getCountryNames', methods=['GET'])
+def getCountryNames(indicator_code=None):
+    args = request.args
+    indicator_code = args['indicator_code']
 
+    sql_query = f"SELECT DISTINCT(country_name) FROM economy_development where indicator_code= '{indicator_code}' ORDER by country_name"
+
+    result_df = pd.read_sql_query(sql_query, engine)
+    return jsonify(list(result_df['country_name']))
+    
 if __name__ == "__main__":
     app.run(debug=True)
